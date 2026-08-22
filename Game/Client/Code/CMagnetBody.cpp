6aee705afd0a59802e42f74e8d1d6729bd93e0b6
qwerasdfzxcv1111 <0dragonplayground0@gmail.com>
2026-08-22 23:58:13 +0900

    [자석 추적 회전 / 자세 보정 구현]
    1. [자석 위치를 카트 기준으로 고정]
    -> 카트의 POS / LOOK / UP 가져옴
    
    Set_Pos({ vCartPos.x, vCartPos.y + 5.f, vCartPos.z + 2.f })
    -> 월드축 기준이라 카트 회전 시 자석 위치가 밀림
    
    Set_Pos({ vCartPos.x, vCartPos.y + 5.f, vCartPos.z + vLook.z * 3.f })
    -> LOOK의 Z값만 사용해서 카트 방향 전체를 반영하지 못함
    
    Set_Pos(vCartPos + vCartLook * 3.f + vCartUp * 5.f)
    -> 카트의 LOOK/UP 기준으로 계산해서 카트 기준 위치를 유지함
    -> 카트가 회전하거나 기울어져도 카트 기준 같은 위치 유지
    
    2. [타겟 방향 계산]
    -> 타겟 위치 - 카트 위치로 vDir 계산
    -> Normalize 처리 해서 방향 벡터의 길이를 1로 맞춰 방향만 정확하게 사용함
    
    3. [기존 GetFollowRotation() 제거]
    -> GetFollowRotation()함수 사용 시 쿼터니언으로 변환하는 과정에서 오류가 발생
    -> 오류 확인 위해 외적 / 내적 / 각도 / 쿼터니언 생성을 Update 안에서 직접 계산하도록 변경
    -> 오류 없는거 보니 아마 GetFollowRoatation이 문제일 수 도 있음
    
    4. 기존 Multiple_Quaternion() 주석처리
    -> Multiple_Quaternion() 함수는 qRot에 회전값 누적이라 경사면 오르고 내릴시 자석 기울어짐 발생
    -> 새 방식은 RIGHT / UP / LOOK으로 최종 자세를 새로 만든 뒤 Set_Quaternion()으로 적용하므로 기존 누적 회전과 같이 사용할 필요가 없어 주석처리함
    
    5. LOOK / UP / RIGHT 재정렬
    -> vFixLook, vFixUp, vFixRight 지역변수 생성
    -> vFixUp 초기값 설정
    -> vDir을 정규화 -> vFixLook 생성
    -> vFixUp × vFixLook ->  vFixRight 생성
    -> vFixLook × vFixRight ->  vFixUp 재계산
    -> vFixLook / vFixUp / vFixRight ->  외적 결과가 서로 수직이기 때문에 직각 방향축으로 만들고, 정규화로 각 축 길이 1로 만듦
    
    7. 회전행렬 함수 생성
    -> D3DX에는 RIGHT / UP / LOOK 3축으로 회전행렬 생성 함수 없으므로
    직접 계산한 3축을 행렬에 각각 넣어 회전행렬 구성
    -> vFixRight / vFixUp / vFixLook을 회전행렬에 적용
    -> 누적으로 틀어진 자세가 남지 않도록 최종 자세를 새로 구성
    
    8. 회전행렬을 쿼터니언으로 변환
    -> D3DXQuaternionRotationMatrix() 사용
    -> 생성된 qFixRot을 Set_Quaternion()으로 자석에 적용 최종 결과
    -> 자석이 타겟의 좌우 / 높낮이를 계속 추적
    -> 높은 곳을 바라본 뒤 다시 내려와도 기울어진 자세가 남지 않도록 보정
    -> 매 프레임 완성된 회전값을 새로 만들기 때문에 Set_Quaternion() 사용 가능

--- Game/Client/Code/CMagnetBody.cpp
+++ Game/Client/Code/CMagnetBody.cpp
@@ -52,14 +52,18 @@ _int CMagnetBody::Update_GameObject(const _float& fDeltaTime)
 	CCartBody* pCartBody = dynamic_cast<CCartBody*>(CManagement::GetInstance()->Find_GameObjectByTag(L"GameLogic", L"Obj_CartBody"));
 	CGameObject* pTargetPos = CManagement::GetInstance()->Find_GameObjectByTag(L"GameLogic", L"Obj_MissileTarget");
 
-	_vec3 vPos, vCartPos, vLook, vDir, vTargetPos;
+	_vec3 vPos, vCartPos, vLook, vDir, vTargetPos, vCartLook, vCartUp;
 	
 	m_pTransformCom->Get_Info(INFO_POS, &vPos);
 	m_pTransformCom->Get_Info(INFO_LOOK, &vLook);
 
 
 	pCartBody->Get_Transform()->Get_Info(INFO_POS, &vCartPos);
-	m_pTransformCom->Set_Pos({ vCartPos.x, vCartPos.y + 5.f, vCartPos.z + 2.f });	//m_pTransformCom->Set_Pos(vCartPos);
+	pCartBody->Get_Transform()->Get_Info(INFO_UP, &vCartUp);
+	pCartBody->Get_Transform()->Get_Info(INFO_LOOK, &vCartLook);
+
+	m_pTransformCom->Set_Pos(vCartPos + vCartLook * 3.f + vCartUp * 5);
+	
 	pTargetPos->Get_Transform()->Get_Info(INFO_POS, &vTargetPos);
 
 	vDir = vTargetPos - vCartPos;
@@ -69,29 +73,60 @@ _int CMagnetBody::Update_GameObject(const _float& fDeltaTime)
 
 	D3DXVec3Normalize(&vDir, &vDir);
 
-	_vec3 vCross;
-	D3DXVec3Cross(&vCross, &vLook, &vDir);
+	//_vec3 vCross;
+	//D3DXVec3Cross(&vCross, &vLook, &vDir);
 
-	if (D3DXVec3Length(&vCross) <= 0.001f)
-		return 0;
+	//if (D3DXVec3Length(&vCross) <= 0.001f)
+	//	return 0;
 
-	D3DXVec3Normalize(&vCross, &vCross);
+	//D3DXVec3Normalize(&vCross, &vCross);
 
-	
-	_float fDot = D3DXVec3Dot(&vLook, &vDir);
+	//
+	//_float fDot = D3DXVec3Dot(&vLook, &vDir);
 
-	if (fDot > 1.f)
-		fDot = 1.f;
+	//if (fDot > 1.f)
+	//	fDot = 1.f;
 
-	else if (fDot < -1.f)
-		fDot = -1.f;
+	//else if (fDot < -1.f)
+	//	fDot = -1.f;
 
-	_float fAngle = acosf(fDot);
+	//_float fAngle = acosf(fDot);
 
-	_quaternion qRot;
-	D3DXQuaternionRotationAxis(&qRot, &vCross, fAngle);
+	//_quaternion qRot;
+	//D3DXQuaternionRotationAxis(&qRot, &vCross, fAngle);
 
-	m_pTransformCom->Multiple_Quaternion(&qRot);
+	//m_pTransformCom->Multiple_Quaternion(&qRot);
+////////////////////////////////////////////////////////////////////// 잘돼면 트랜스폼에 함수생성해서 불러오는 식
+	_vec3 vFixUp, vFixRight, vFixLook;
+	vFixUp = { 0.f, 1.f, 0.f };			// 기준값
+
+	D3DXVec3Normalize(&vFixLook, &vDir);
+
+	D3DXVec3Cross(&vFixRight, &vFixUp, &vFixLook);
+	D3DXVec3Cross(&vFixUp, &vFixLook, &vFixRight);
+
+	D3DXVec3Normalize(&vFixUp, &vFixUp);
+	D3DXVec3Normalize(&vFixRight, &vFixRight);
+
+	_matrix matFixRot;
+	D3DXMatrixIdentity(&matFixRot);
+
+	matFixRot._11 = vFixRight.x;
+	matFixRot._12 = vFixRight.y;
+	matFixRot._13 = vFixRight.z;
+
+	matFixRot._21 = vFixUp.x;
+	matFixRot._22 = vFixUp.y;
+	matFixRot._23 = vFixUp.z;
+
+	matFixRot._31 = vFixLook.x;
+	matFixRot._32 = vFixLook.y;
+	matFixRot._33 = vFixLook.z;
+
+	_quaternion qFixRot;
+	D3DXQuaternionRotationMatrix(&qFixRot, &matFixRot);
+
+	m_pTransformCom->Set_Quaternion(&qFixRot);
 
 	return CGameObject::Update_GameObject(fDeltaTime);
 }
